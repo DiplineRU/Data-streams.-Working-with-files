02e646a15ea040cec7342fca09f4b18daee5def9
DiplineRU <artgul52@gmail.com>
2025-02-22 12:51:47 +0300

    init commit

---
+++ src/main/java/com/example/Data_streams_Working_with_files/repository/FacultyRepository.java
@@ -0,0 +1,16 @@
+package com.example.Data_streams_Working_with_files.repository;
+
+import com.example.Data_streams_Working_with_files.model.Faculty;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+
+@Repository
+public interface FacultyRepository extends JpaRepository<Faculty, Long> {
+
+    Collection<Faculty> findByNameIgnoreCase(String name);
+
+    Collection<Faculty> findByColorIgnoreCase(String color);
+
+}
